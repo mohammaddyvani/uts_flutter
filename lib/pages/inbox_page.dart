3a6e9e20b7666b77e6746ad943b70b2770ae0f25
FlutLab.io <>
2022-04-11 07:58:08 +0000

    Autosave

--- lib/pages/inbox_page.dart
+++ lib/pages/inbox_page.dart
@@ -3,28 +3,6 @@ import 'package:uts/pages/sidebar.dart';
 import 'package:uts/pages/message/msg.dart';
 
 class InboxPage extends StatelessWidget {
-  List input = [
-    [
-      'Andi',
-      'Hello there!!',
-      'Has been a long time....'
-    ],
-    [
-      'Zaki',
-      'Important News',
-      'That is awsome!! Take a look...'
-    ],
-    [
-      'Ivan',
-      'Material Desain',
-      'There is the widget library...'
-    ],
-    [
-      'Andi',
-      'Weekend Meeting?',
-      'Can you meet this weekend?...'
-    ],
-  ];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -46,14 +24,18 @@ class InboxPage extends StatelessWidget {
             children: [
               const Text('Today', style: TextStyle(color: Colors.black)),
               const SizedBox(height: 5.0),
-              Pesan(nama: "Andi", pesan1: "Hallo Semua!!", pesan2: "Selamat siang, saya aad..."),
-              Pesan(nama: "Andi", pesan1: "Hallo Semua!!", pesan2: "Selamat siang, saya aad..."),
-              Pesan(nama: "Andi", pesan1: "Hallo Semua!!", pesan2: "Selamat siang, saya aad..."),
-              Pesan(nama: "Andi", pesan1: "Hallo Semua!!", pesan2: "Selamat siang, saya aad..."),
-              Pesan(nama: "Andi", pesan1: "Hallo Semua!!", pesan2: "Selamat siang, saya aad..."),
-              Pesan(nama: "Andi", pesan1: "Hallo Semua!!", pesan2: "Selamat siang, saya aad..."),
-              Pesan(nama: "Andi", pesan1: "Hallo Semua!!", pesan2: "Selamat siang, saya aad..."),
-              Pesan(nama: "Andi", pesan1: "Hallo Semua!!", pesan2: "Selamat siang, saya aad..."),
+              Pesan(nama: "Andi", pesan1: "Hello there!!", pesan2: "Has been a long time...."),
+              Pesan(nama: "Zaki", pesan1: "Important News", pesan2: "That is awsome!! Take a look..."),
+              Pesan(nama: "Ivan", pesan1: "Material Desain", pesan2: "There is the widget library..."),
+              Pesan(nama: "Bagus", pesan1: "Weekend Meeting?", pesan2: "Can you meet this weekend?..."),
+              Pesan(nama: "Bagas", pesan1: "Hello there!!", pesan2: "Has been a long time...."),
+              Pesan(nama: "Riki", pesan1: "Important News", pesan2: "That is awsome!! Take a look..."),
+              Pesan(nama: "Wildan", pesan1: "Material Desain", pesan2: "There is the widget library..."),
+              Pesan(nama: "Ahmad", pesan1: "Weekend Meeting?", pesan2: "Can you meet this weekend?..."),
+              Pesan(nama: "Dyvani", pesan1: "Hello there!!", pesan2: "Has been a long time...."),
+              Pesan(nama: "Wahyu", pesan1: "Important News", pesan2: "That is awsome!! Take a look..."),
+              Pesan(nama: "Mirza", pesan1: "Material Desain", pesan2: "There is the widget library..."),
+              Pesan(nama: "Septian", pesan1: "Weekend Meeting?", pesan2: "Can you meet this weekend?..."),
             ],
           ),
         ),
