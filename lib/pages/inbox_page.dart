13a7db8a7d24920b5d6f3f581ed381112ec406a6
FlutLab.io <>
2022-04-11 07:47:21 +0000

    Autosave

--- lib/pages/inbox_page.dart
+++ lib/pages/inbox_page.dart
@@ -1,10 +1,30 @@
-import 'dart:html';
-
 import 'package:flutter/material.dart';
 import 'package:uts/pages/sidebar.dart';
 import 'package:uts/pages/message/msg.dart';
 
 class InboxPage extends StatelessWidget {
+  List input = [
+    [
+      'Andi',
+      'Hello there!!',
+      'Has been a long time....'
+    ],
+    [
+      'Zaki',
+      'Important News',
+      'That is awsome!! Take a look...'
+    ],
+    [
+      'Ivan',
+      'Material Desain',
+      'There is the widget library...'
+    ],
+    [
+      'Andi',
+      'Weekend Meeting?',
+      'Can you meet this weekend?...'
+    ],
+  ];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -26,14 +46,23 @@ class InboxPage extends StatelessWidget {
             children: [
               const Text('Today', style: TextStyle(color: Colors.black)),
               const SizedBox(height: 5.0),
-              Pesan(nama: "Andi", pesan1: "Hallo Semua!!", pesan2: "Selamat siang, saya aad..."),
-              Pesan(nama: "Andi", pesan1: "Hallo Semua!!", pesan2: "Selamat siang, saya aad..."),
-              Pesan(nama: "Andi", pesan1: "Hallo Semua!!", pesan2: "Selamat siang, saya aad..."),
-              Pesan(nama: "Andi", pesan1: "Hallo Semua!!", pesan2: "Selamat siang, saya aad..."),
-              Pesan(nama: "Andi", pesan1: "Hallo Semua!!", pesan2: "Selamat siang, saya aad..."),
-              Pesan(nama: "Andi", pesan1: "Hallo Semua!!", pesan2: "Selamat siang, saya aad..."),
-              Pesan(nama: "Andi", pesan1: "Hallo Semua!!", pesan2: "Selamat siang, saya aad..."),
-              Pesan(nama: "Andi", pesan1: "Hallo Semua!!", pesan2: "Selamat siang, saya aad..."),
+              ListView.builder(
+                  itemCount: input.length,
+                  itemBuilder: (context, index) {
+                    return Column(
+                      children: [
+                        Pesan(nama: input[index][0][0], pesan1: input[index][1][0], pesan2: input[index][0][1]),
+                      ],
+                    );
+                  }),
+              // Pesan(nama: "Andi", pesan1: "Hallo Semua!!", pesan2: "Selamat siang, saya aad..."),
+              // Pesan(nama: "Andi", pesan1: "Hallo Semua!!", pesan2: "Selamat siang, saya aad..."),
+              // Pesan(nama: "Andi", pesan1: "Hallo Semua!!", pesan2: "Selamat siang, saya aad..."),
+              // Pesan(nama: "Andi", pesan1: "Hallo Semua!!", pesan2: "Selamat siang, saya aad..."),
+              // Pesan(nama: "Andi", pesan1: "Hallo Semua!!", pesan2: "Selamat siang, saya aad..."),
+              // Pesan(nama: "Andi", pesan1: "Hallo Semua!!", pesan2: "Selamat siang, saya aad..."),
+              // Pesan(nama: "Andi", pesan1: "Hallo Semua!!", pesan2: "Selamat siang, saya aad..."),
+              // Pesan(nama: "Andi", pesan1: "Hallo Semua!!", pesan2: "Selamat siang, saya aad..."),
             ],
           ),
         ),
